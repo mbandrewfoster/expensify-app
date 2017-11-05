63b92c9e3137c9fa7edcbac6c5e4d2b006ac8ba3
Andrew Foster <andrew.foster@marketboomer.com>
2017-11-05 13:18:08 +1100

    Fix incorrect import of PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,7 +8,7 @@ import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
