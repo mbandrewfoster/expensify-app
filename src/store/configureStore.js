0f8113cf2d99e62dc685ae34a43368215fb22586
Andrew Foster <andrew.foster@marketboomer.com>
2017-11-05 21:53:58 +1100

    Fix error when browser does not have redux dev tools installed

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,10 @@ import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
 import authReducer from '../reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = 
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :
+  compose
 
 export default () => {
   const store = createStore(
